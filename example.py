7e8808924c01f2bc18712bd484f6dd29b81d3567
unsubstantiated-Script <jusfulton@gmail.com>
2024-05-10 18:59:06 -0700

    Q Widget Done

--- example.py
+++ example.py
@@ -1,25 +1,43 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, QLineEdit
+from datetime import date, datetime
+
+from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton
 
 
 class AgeCalculator(QWidget):
     def __init__(self):
         # Doing this to initialize the parent
         super().__init__()
+        self.setWindowTitle('Age Calculator')
         grid = QGridLayout()
 
+        # Creat Widgets
         name_label = QLabel("Name")
-        name_line_edit = QLineEdit()
+        self.name_line_edit = QLineEdit()
 
         date_label = QLabel("Date of Birth MM/DD/YYYY")
-        date_birth_line_edit = QLineEdit()
+        self.date_birth_line_edit = QLineEdit()
 
+        calculate__button = QPushButton("Calculate Age")
+        calculate__button.clicked.connect(self.calculate_age)
+        self.output_label = QLabel("")
+
+        # Add widgets to grid
         grid.addWidget(name_label, 0, 0)
-        grid.addWidget(name_line_edit, 0, 1)
+        grid.addWidget(self.name_line_edit, 0, 1)
         grid.addWidget(date_label, 1, 0)
-        grid.addWidget(date_birth_line_edit, 1, 1)
+        grid.addWidget(self.date_birth_line_edit, 1, 1)
+        grid.addWidget(calculate__button, 2, 0, 1, 2)
+        grid.addWidget(self.output_label, 3, 0, 1, 2)
 
         self.setLayout(grid)
+
+    def calculate_age(self):
+        current_year = datetime.now().year
+        date_of_birth = self.date_birth_line_edit.text()
+        year_of_birth = datetime.strptime(date_of_birth, "%m/%d/%Y").year
+        age = current_year - year_of_birth
+        self.output_label.setText(f"{self.name_line_edit.text()} is {age} years old")
 
 
 app = QApplication(sys.argv)
